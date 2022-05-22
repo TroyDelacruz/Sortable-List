e12390c5ca8f1b7d1c1703160a648d63af9a0e74
Troy Delacruz <troy8076@yahoo.com>
2022-05-22 01:32:50 -0400

    Finish adding functionality to list

--- scripts.js
+++ scripts.js
@@ -53,7 +53,35 @@ function createList() {
 }
 
 function dragStart() {
-    
+    dragStartIndex = +this.closest('li').getAttribute('data-index');
+}
+
+function dragEnter() {
+    this.lassList.add('over');
+}
+
+function dragLeave() {
+    this.classList.remove('over');
+}
+
+function dragOver(e) {
+    e.preventDefault();
+}
+
+
+function dragDrop() {
+    const dragEndIndex = +this.getAttribute('data-index');
+    swapItems(dragStartIndex, dragEndIndex);
+
+    this.classList.remove('over');
+}
+
+function swapItems(fromIndex, toIndex) {
+    const itemOne = listItems[fromIndex].querySelector('.draggable');
+    const itemTwo = listItems[toIndex].querySelector('.draggable');
+
+    listItems[fromIndex].appendChild(itemTwo);
+    listItems[toIndex].appendChild(itemOne);
 }
 
 
