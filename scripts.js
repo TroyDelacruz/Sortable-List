1ddebffda583b2e286817fc1d2a5400f97f0b12d
Troy Delacruz <troy8076@yahoo.com>
2022-05-22 00:33:08 -0400

    Add function for jrpg list

---
+++ scripts.js
@@ -0,0 +1,45 @@
+// Select global variables for list
+const draggable_list = document.getElementById('draggable-list');
+const check = document.getElementById('check');
+
+
+// Array for the sortable list
+const bestJrpgs = [
+    'Tales of Symphonia',
+    'Elden Ring',
+    'Legend of Heroes: Trails of Cold Steel',
+    'Kirby and the Forgotten Land',
+    'Legend of Zelda: Breath of the Wild',
+    'Dark Souls II',
+    'Tales of Arise',
+    'Fire Emblem: Radiant Dawn',
+    'Fire Emblem: Awakening',
+    'Final Fantasy XIV',
+];
+
+// Store the list items
+
+const listItems = [];
+
+let dragStartIndex;
+
+createList();
+
+// Insert list items into DOM
+
+function createList() {
+    [...bestJrpgs]
+      .forEach((person, index) => {
+          const listItem = document.createElement('li');
+
+          listItem.setAttribute('data-index', index);
+
+          listItem.innerHTML = `
+          <span class="number">${index + 1} </span>
+          <div class="draggable" draggable="true">
+            <p class="person-name">${person}</p>
+            <i class="fas fa-grip-lines">
+          </div>
+          `;
+      });
+}
